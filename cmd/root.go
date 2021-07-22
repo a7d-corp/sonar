1544dfc9a57ce57f5724e10874a818b120af2c5b
Simon Weald <glitchcrab-github@simonweald.com>
2021-07-22 22:07:37 +0100

    disable env var config for now

--- cmd/root.go
+++ cmd/root.go
@@ -80,7 +80,7 @@ func initConfig() {
 		namespace = "default"
 	}
 
-	viper.AutomaticEnv() // read in environment variables that match
+	//viper.AutomaticEnv() // read in environment variables that match
 
 	// If a config file is found, read it in.
 	if err := viper.ReadInConfig(); err == nil {
