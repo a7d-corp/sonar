ed7b767b1e948cd99eb3415e18397b335bc07740
Simon Weald <glitchcrab-github@simonweald.com>
2021-07-27 22:49:13 +0100

    add k8sclient package

---
+++ service/k8sclient/k8sclient.go
@@ -0,0 +1,50 @@
+/*
+Copyright © 2021 Simon Weald
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package k8sclient
+
+import (
+	"path/filepath"
+
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/util/homedir"
+
+	log "github.com/sirupsen/logrus"
+)
+
+func New(kubeContext string, kubeConfig *string) (kubernetes.Interface, error) {
+	// Set the kubeconfig to the default location if the path
+	// wasn't provided.
+	if *kubeConfig == "" {
+		if home := homedir.HomeDir(); home != "" {
+			*kubeConfig = filepath.Join(home, ".kube", "config")
+		}
+	}
+
+	// TODO: implement switching to provided context name
+
+	config, err := clientcmd.BuildConfigFromFlags("", *kubeConfig)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return clientset, err
+}
