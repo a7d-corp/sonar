0cb09681f5aea85db08240afe4324b7eb3b52551
Simon Weald <glitchcrab-github@simonweald.com>
2021-08-04 22:43:39 +0100

    add project details

---
+++ pkg/project/project.go
@@ -0,0 +1,24 @@
+package project
+
+var (
+	description = "Sonar deploys a debugging container to a Kubernetes cluster."
+	name        = "sonar"
+	source      = "https://github.com/glitchcrab/sonar"
+	version     = "0.1.0"
+)
+
+func Description() string {
+	return description
+}
+
+func Name() string {
+	return name
+}
+
+func Source() string {
+	return source
+}
+
+func Version() string {
+	return version
+}
