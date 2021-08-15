72269dea3d662b9f23ca6d100ad947ab0eeb18ad
GitHub Action <action@github.com>
2021-08-15 17:16:13 +0000

    Bump version to 0.5.1-dev

--- pkg/project/project.go
+++ pkg/project/project.go
@@ -4,7 +4,7 @@ var (
 	description = "Sonar deploys a debugging container to a Kubernetes cluster."
 	name        = "sonar"
 	source      = "https://github.com/glitchcrab/sonar"
-	version     = "0.5.0"
+	version     = "0.5.1-dev"
 )
 
 func Description() string {
