0e6ec643f3f2f64c1bef5aa1d9d5d58219fd28f1
Simon Weald <glitchcrab-github@simonweald.com>
2025-07-22 11:56:12 +0100

    Read shell KUBECONFIG env var if kubeconfig flag not provided

--- cmd/root.go
+++ cmd/root.go
@@ -5,7 +5,7 @@ Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 
-    http://www.apache.org/licenses/LICENSE-2.0
+	http://www.apache.org/licenses/LICENSE-2.0
 
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
@@ -55,6 +55,8 @@ Global flags:
 --kubeconfig (default: '/home/$user/.kube/config')
 
 Absolute path to the kubeconfig file to use.
+If left blank, Sonar will read the KUBECONFIG environment variable.
+If that is not set, Sonar will use the default kubeconfig file location.
 
 --context (default: current context in kube config)
 
