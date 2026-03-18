2982632d2d4117909b68b669fdc3efca08d2e3c9
Simon Weald <glitchcrab-github@simonweald.com>
2026-03-18 06:56:49 +0000

    Add pod status to ls command output

--- internal/types/types.go
+++ internal/types/types.go
@@ -1,7 +1,12 @@
 package types
 
+import (
+	corev1 "k8s.io/api/core/v1"
+)
+
 // DiscoveredPod represents a pod which is a candidate for execing into.
 type DiscoveredPod struct {
 	Name      string
 	Namespace string
+	Status    corev1.PodPhase
 }
