3733e78c87392e643306c7d998a81db5a598ce07
Simon Weald <glitchcrab-github@simonweald.com>
2026-03-27 19:18:10 +0000

    formatting

--- service/k8sresource/delete_deployment.go
+++ service/k8sresource/delete_deployment.go
@@ -5,7 +5,7 @@ Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 
-    http://www.apache.org/licenses/LICENSE-2.0
+	http://www.apache.org/licenses/LICENSE-2.0
 
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
@@ -19,8 +19,8 @@ import (
 	"context"
 	"fmt"
 
-	"github.com/glitchcrab/sonar/internal/helpers"
 	"github.com/glitchcrab/sonar/internal/clientconfigs"
+	"github.com/glitchcrab/sonar/internal/helpers"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 )
