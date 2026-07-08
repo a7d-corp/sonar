bc2864fd289d0f79be4b2a6a12ec268402a67647
Simon Weald <glitchcrab-github@simonweald.com>
2026-07-08 21:16:45 +0100

    remove ineffectual assignment

--- cmd/destroy/command.go
+++ cmd/destroy/command.go
@@ -24,6 +24,7 @@ import (
 	"github.com/glitchcrab/sonar/internal/app"
 	"github.com/glitchcrab/sonar/internal/config"
 	"github.com/glitchcrab/sonar/internal/k8sclient"
+	sonartypes "github.com/glitchcrab/sonar/internal/types"
 	"github.com/glitchcrab/sonar/internal/utils"
 	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
@@ -118,7 +119,8 @@ func runDeleteCommand(cmd *cobra.Command, args []string) error {
 	ctx := context.TODO()
 
 	// Find all Sonar deployments.
-	discoveredDeployments, err := utils.FindSonarDeployments(k8sClientSet, ctx, a.Globals.Name, searchNamespace, searchLabels)
+	var discoveredDeployments []sonartypes.DiscoveredDeployment
+	discoveredDeployments, err = utils.FindSonarDeployments(k8sClientSet, ctx, a.Globals.Name, searchNamespace, searchLabels)
 
 	var selectedDeploy string
 	if !skipInteractiveLookup {
