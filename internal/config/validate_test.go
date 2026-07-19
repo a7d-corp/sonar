7422413d789be81babde79d82517d345e116cda6
Simon Weald <glitchcrab-github@simonweald.com>
2026-07-19 21:07:18 +0100

    simplify error handling

--- internal/config/validate_test.go
+++ internal/config/validate_test.go
@@ -42,10 +42,8 @@ func TestValidateGlobalConfig(t *testing.T) {
 	for _, testCase := range testCases {
 		t.Run(testCase.name, func(t *testing.T) {
 			errs := ValidateGlobalConfig(testCase.input)
-			gotErr := false
-			if errs != nil {
-				gotErr = true
-			}
+			gotErr := errs != nil
+
 			if gotErr != testCase.wantErr {
 				t.Errorf("error expected: %t, got %t, %v", testCase.wantErr, gotErr, errs)
 			}
