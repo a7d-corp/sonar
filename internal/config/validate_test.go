42568f912cf1af6368da5290abecb7736ea963ed
Simon Weald <glitchcrab-github@simonweald.com>
2026-07-23 20:29:04 +0100

    extend testing to cover the mutating Globals struct

--- internal/config/validate_test.go
+++ internal/config/validate_test.go
@@ -2,40 +2,172 @@ package config
 
 import (
 	"testing"
+
+	"github.com/go-test/deep"
 )
 
 func TestValidateGlobalConfig(t *testing.T) {
 	testCases := []struct {
-		input   *Globals
-		name    string
-		wantErr bool
+		input           *Globals
+		output          *Globals
+		name            string
+		validateGlobals bool
+		wantErr         bool
 	}{
+		// Test name validation
 		{
 			name: "test valid name",
 			input: &Globals{
-				Name:      "valid-name",
-				Labels:    make(map[string]string),
+				Name: "valid-name",
+				Labels: map[string]string{
+					"owner":      "sonar",
+					"created-by": "sonar",
+				},
 				Namespace: "default",
 			},
-			wantErr: false,
+			output: &Globals{
+				Name:     "valid-name",
+				FullName: "sonar-valid-name",
+				Labels: map[string]string{
+					"name":       "valid-name",
+					"owner":      "sonar",
+					"created-by": "sonar",
+				},
+				Namespace: "default",
+			},
+			validateGlobals: true,
+			wantErr:         false,
+		},
+		{
+			name: "test exact-length name",
+			input: &Globals{
+				Name: "duhuddmhpmitxtevvixngwmnbwjmqbrdqquuiknfqxfnrwybdh",
+				Labels: map[string]string{
+					"owner":      "sonar",
+					"created-by": "sonar",
+				},
+				Namespace: "default",
+			},
+			output: &Globals{
+				Name:     "duhuddmhpmitxtevvixngwmnbwjmqbrdqquuiknfqxfnrwybdh",
+				FullName: "sonar-duhuddmhpmitxtevvixngwmnbwjmqbrdqquuiknfqxfnrwybdh",
+				Labels: map[string]string{
+					"name":       "duhuddmhpmitxtevvixngwmnbwjmqbrdqquuiknfqxfnrwybdh",
+					"owner":      "sonar",
+					"created-by": "sonar",
+				},
+				Namespace: "default",
+			},
+			validateGlobals: true,
+			wantErr:         false,
 		},
 		{
 			name: "test over-length name",
 			input: &Globals{
-				Name:      "iexoochohNgoughie1wu8Ahyoof7de0iequiuzoo0mic8siasdc",
-				Labels:    make(map[string]string),
+				Name: "duhuddmhpmitxtevvixngwmnbwjmqbrdqquuiknfqxfnrwybdh1",
+				Labels: map[string]string{
+					"owner":      "sonar",
+					"created-by": "sonar",
+				},
 				Namespace: "default",
 			},
-			wantErr: true,
+			output: &Globals{
+				Name: "duhuddmhpmitxtevvixngwmnbwjmqbrdqquuiknfqxfnrwybdh1",
+				Labels: map[string]string{
+					"owner":      "sonar",
+					"created-by": "sonar",
+				},
+				Namespace: "default",
+			},
+			validateGlobals: false,
+			wantErr:         true,
 		},
 		{
 			name: "test name with invalid characters",
 			input: &Globals{
-				Name:      "invalid_name",
-				Labels:    make(map[string]string),
+				Name: "invalid_name",
+				Labels: map[string]string{
+					"owner":      "sonar",
+					"created-by": "sonar",
+				},
 				Namespace: "default",
 			},
-			wantErr: true,
+			output: &Globals{
+				Name: "invalid_name",
+				Labels: map[string]string{
+					"owner":      "sonar",
+					"created-by": "sonar",
+				},
+				Namespace: "default",
+			},
+			validateGlobals: false,
+			wantErr:         true,
+		},
+		{
+			name: "test no provided name",
+			input: &Globals{
+				Name: "",
+				Labels: map[string]string{
+					"owner":      "sonar",
+					"created-by": "sonar",
+				},
+				Namespace: "default",
+			},
+			output: &Globals{
+				Name:     "sonar",
+				FullName: "sonar",
+				Labels: map[string]string{
+					"name":       "sonar",
+					"owner":      "sonar",
+					"created-by": "sonar",
+				},
+				Namespace: "default",
+			},
+			validateGlobals: true,
+			wantErr:         false,
+		},
+		// Test namespace validation
+		{
+			name: "test valid namespace",
+			input: &Globals{
+				Name: "valid-name",
+				Labels: map[string]string{
+					"owner":      "sonar",
+					"created-by": "sonar",
+				},
+				Namespace: "valid-namespace",
+			},
+			output: &Globals{
+				Name: "valid-name",
+				Labels: map[string]string{
+					"name":       "valid-name",
+					"created-by": "sonar",
+				},
+				Namespace: "valid-namespace",
+			},
+			validateGlobals: false,
+			wantErr:         false,
+		},
+		{
+			name: "test namespace with invalid characters",
+			input: &Globals{
+				Name: "valid-name",
+				Labels: map[string]string{
+					"owner":      "sonar",
+					"created-by": "sonar",
+				},
+				Namespace: "invalid_namespace",
+			},
+			output: &Globals{
+				Name: "valid-name",
+				Labels: map[string]string{
+					"owner":      "sonar",
+					"created-by": "sonar",
+				},
+				Namespace: "invalid_namespace",
+			},
+			validateGlobals: false,
+			wantErr:         true,
 		},
 	}
 
@@ -46,6 +178,12 @@ func TestValidateGlobalConfig(t *testing.T) {
 
 			if gotErr != testCase.wantErr {
 				t.Errorf("error expected: %t, got %t, %v", testCase.wantErr, gotErr, errs)
+			}
+
+			if testCase.validateGlobals {
+				if diff := deep.Equal(*testCase.input, *testCase.output); diff != nil {
+					t.Error(diff)
+				}
 			}
 		})
 	}
